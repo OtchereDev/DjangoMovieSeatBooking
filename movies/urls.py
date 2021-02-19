b7cb09452c6f9fe413cb48f031bdbb86e55e5294
Oliver Otchere <oliverotchere4@gmail.com>
2021-02-19 12:36:52 +0000

    added url for verification of free seats

--- movies/urls.py
+++ movies/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import index
+from .views import index,validateSeats
 
 app_name='movies'
 
 urlpatterns = [
-    path('',index,name='home')
+    path('',index,name='home'),
+    path('check_seats/',validateSeats,name='check_seats'),
 ]
 
