a9ac0e764ea26a6e81e43c9cd3ba36af10e52f21
Oliver Otchere <oliverotchere4@gmail.com>
2021-02-23 21:57:40 +0000

    refactor js to use on function to contact api

--- static/backend.js
+++ static/backend.js
@@ -2,17 +2,20 @@
 const seats_requested= localStorage.getItem('selectedSeats')? JSON.parse(localStorage.getItem('selectedSeats')) : null
 const movie_title=movieSelect.options[movieSelect.selectedIndex].id
 
-const seat_list=seats_requested.map(seat=> seat+1 )
+const seats_list=seats_requested.map(seat=> seat+1 )
 
-fetch('check_seats/',{
-    method:'POST',
-    headers:{
-        'Content-Type': 'application/json'
-    },
-    body:JSON.stringify({
-        'seat_numbers':seat_list,
-        'movie_title':movie_title
+async function contactAPI(url,body){
+    const response=await fetch(url,{
+        method:'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify(body)
     })
-}).then(e=>{
+
+    return response.json()
+}
+
+contactAPI('/check_seats/',{seats_list,movie_title}).then(e=>{
     console.log(e)
 })
