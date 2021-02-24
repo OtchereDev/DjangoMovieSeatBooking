dee2d03770c35d47a75e08ea6143a1f841fbd321
Oliver Otchere <oliverotchere4@gmail.com>
2021-02-24 08:42:10 +0000

    added payment and occupied endpoint

--- movies/urls.py
+++ movies/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
-from .views import index,validateSeats,webhook
+from .views import index,validateSeats,webhook,occupiedSeats,makePayment
 
 app_name='movies'
 
 urlpatterns = [
     path('',index,name='home'),
     path('check_seats/',validateSeats,name='check_seats'),
+    path('occupied/',occupiedSeats,name='occupied'),
     path('webhook/',webhook,name='webhook'),
+    path('payment/',makePayment,name='make-payment')
 ]
 
