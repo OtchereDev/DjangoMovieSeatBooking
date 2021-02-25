b3049490e368c11f5cda9df8cff9c155cc84f44e
Oliver Otchere <oliverotchere4@gmail.com>
2021-02-25 08:18:00 +0000

    added paymenIntent and payment model to db

--- movies/admin.py
+++ movies/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Movie,Seat
+from .models import Movie,Seat,Payment,PaymentIntent
 
 admin.site.register(Movie)
 admin.site.register(Seat)
+admin.site.register(Payment)
+admin.site.register(PaymentIntent)
